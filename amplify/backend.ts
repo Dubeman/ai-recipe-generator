0c532a234c3b693684739380d0da46c62525e5d4
md8889 <dubemanas10@gmail.com>
2025-01-30 20:59:15 -0500

    connect to bedrock

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,39 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { data } from "./data/resource";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { auth } from "./auth/resource";
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+/**
+ * Add a Bedrock data source to the backend. This is used to call the Bedrock model. We use the
+ * GraphQL API to call the Bedrock model because it's the easiest way to get the data from the model.
+ */
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+  "bedrockDS",
+  "https://bedrock-runtime.us-east-1.amazonaws.com",
+  {
+    authorizationConfig: {
+      signingRegion: "us-east-1",
+      signingServiceName: "bedrock",
+    },
+  }
+);
+
+/**
+ * Grant the Bedrock data source access to the Bedrock model.
+ */
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
+    ],
+    actions: ["bedrock:InvokeModel"],
+    
+  })
+);
